cbf559f37ce749e383817a799120060cf71ff405
HaiTran25 <haitlhe161311@fpt.edu.vn>
2023-10-08 21:59:29 +0700

    Login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginFormComponent} from "./features/login/login-form/login-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginFormComponent
+  },
+  {
+    path: '',
+    component: LoginFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
