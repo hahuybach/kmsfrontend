8ed0e84d4536b8360874e25bc8222dadf240cc65
HaiTran25 <haitlhe161311@fpt.edu.vn>
2023-12-05 01:15:05 +0700

    Update user detail UX UI

--- src/app/features/post-login/user/user-detail/user-detail.component.ts
+++ src/app/features/post-login/user/user-detail/user-detail.component.ts
@@ -6,70 +6,72 @@ import {Role} from "../../../../shared/enum/role";
 import {AuthService} from "../../../../services/auth.service";
 
 @Component({
-    selector: 'app-user-detail',
-    templateUrl: './user-detail.component.html',
-    styleUrls: ['./user-detail.component.scss']
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
-    user: UserResponseForUserList;
-    isPrincipal: boolean;
-    isAdmin: boolean;
-    isDirector: boolean
+  user: UserResponseForUserList;
+  isPrincipal: boolean;
+  isAdmin: boolean;
+  isDirector: boolean
+  isActives: any[] = [{label: 'Đang hoạt động', value: 'true'},
+    {label: 'Ngưng hoạt động', value: 'false'}]
 
-    constructor(private accountService: AccountService,
-                private router: Router,
-                private activateRouter: ActivatedRoute,
-                private auth: AuthService) {
+  constructor(private accountService: AccountService,
+              private router: Router,
+              private activateRouter: ActivatedRoute,
+              private auth: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.setAuthority();
+    this.accountService.findById(this.activateRouter.snapshot.paramMap.get('id'))
+      .subscribe(
+        {
+          next: (data) => {
+            this.user = data.userDto;
+            console.log(this.user);
+          }
+        }
+      )
+
+  }
+
+  setAuthority() {
+    for (const argument of this.auth.getRoleFromJwt()) {
+      if (argument.authority === Role.PRINCIPAL) {
+        this.isPrincipal = true;
+      }
+      if (argument.authority === Role.ADMIN) {
+        this.isAdmin = true;
+      }
+      if (argument.authority === Role.DIRECTOR) {
+        this.isDirector = true;
+      }
     }
+  }
 
-    ngOnInit(): void {
-        this.setAuthority();
-        this.accountService.findById(this.activateRouter.snapshot.paramMap.get('id'))
-            .subscribe(
-                {
-                    next: (data) => {
-                        this.user = data.userDto;
-                        console.log(this.user);
-                    }
-                }
-            )
-
+  isUpdatable(updateUser: UserResponseForUserList) {
+    if (updateUser.accountRoles?.some(value => value.role?.roleName === Role.ADMIN)) {
+      return false;
     }
-
-    setAuthority() {
-        for (const argument of this.auth.getRoleFromJwt()) {
-            if (argument.authority === Role.PRINCIPAL) {
-                this.isPrincipal = true;
-            }
-            if (argument.authority === Role.ADMIN) {
-                this.isAdmin = true;
-            }
-            if (argument.authority === Role.DIRECTOR) {
-                this.isDirector = true;
-            }
-        }
-    }
-
-    isUpdatable(updateUser: UserResponseForUserList) {
-        if (updateUser.accountRoles?.some(value => value.role?.roleName === Role.ADMIN)) {
-            return false;
-        }
-        if (updateUser.school?.schoolId == 1 && (this.isDirector || this.isAdmin)) {
-            if (this.isDirector && updateUser.accountRoles?.some(value => value.role?.roleName === Role.DIRECTOR)) {
-                return false;
-            }
-            return true;
-        }
-        if (updateUser.school?.schoolId != 1 && (this.isAdmin || this.isPrincipal)) {
-            if (this.isPrincipal && updateUser.accountRoles?.some(value => value.role?.roleName === Role.PRINCIPAL)) {
-                return false;
-            }
-            return true;
-        }
+    if (updateUser.school?.schoolId == 1 && (this.isDirector || this.isAdmin)) {
+      if (this.isDirector && updateUser.accountRoles?.some(value => value.role?.roleName === Role.DIRECTOR)) {
         return false;
+      }
+      return true;
     }
+    if (updateUser.school?.schoolId != 1 && (this.isAdmin || this.isPrincipal)) {
+      if (this.isPrincipal && updateUser.accountRoles?.some(value => value.role?.roleName === Role.PRINCIPAL)) {
+        return false;
+      }
+      return true;
+    }
+    return false;
+  }
 
-    onUpdate() {
-        this.router.navigate(['user/update' + this.user.userId])
-    }
+  onUpdate() {
+    this.router.navigate(['user/update' + this.user.userId])
+  }
 }
