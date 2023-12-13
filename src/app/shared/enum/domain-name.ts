26a4241f9a4c1fde6543d1fe3c47beb954d55469
Duylk411 <105983032+Duylk411@users.noreply.github.com>
2023-12-13 15:07:14 +0700

    deploy http

--- src/app/shared/enum/domain-name.ts
+++ src/app/shared/enum/domain-name.ts
@@ -2,5 +2,5 @@ export enum DomainName{
   // with the slash
   // URL = "http://159.223.58.220:8080/",
   // URL = "http://localhost:8080/",
-  URL = "https://kms.edu.vn:8080/"
+  URL = "http://kms.edu.vn:8080/"
 }
